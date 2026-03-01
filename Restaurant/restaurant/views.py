5ad870bb89ab3f7bb8a63bf3339b8211867be564
NanaB <nanaburdzenidze@icloud.com>
2026-03-01 23:30:08 +0400

    Update views.py

--- Restaurant/restaurant/views.py
+++ Restaurant/restaurant/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 
 API_BASE = "https://restaurant.stepprojects.ge"
 def index(request):
-    ...
-
+    categories = request.get(f"{API_BASE}/api/Categories/GetAll")
+    products = request.get(f"{API_BASE}/api/Products/GetAll")
+    sel_category = request.GET.get('category')
+    if sel_category is None:
+        ...
+    
+    
+    context = {
+        'categories' : categories,
+        'products' : products,
+        #'prod_count' : count,
+    }
+        
 def cart_page(request):
     ...
+
     
