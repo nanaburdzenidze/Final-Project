14c4183e118881adcb3ccfe7dc0d99ccecee3d7b
NanaB <nanaburdzenidze@icloud.com>
2026-03-02 00:05:05 +0400

    .

--- Restaurant/restaurant/views.py
+++ Restaurant/restaurant/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+import requests
 
 API_BASE = "https://restaurant.stepprojects.ge"
 def index(request):
-    categories = request.get(f"{API_BASE}/api/Categories/GetAll")
-    products = request.get(f"{API_BASE}/api/Products/GetAll")
+    categories = requests.get(f"{API_BASE}/api/Categories/GetAll").json()
+    products = requests.get(f"{API_BASE}/api/Products/GetAll").json()
     sel_category = request.GET.get('category')
     if sel_category is None:
         ...
